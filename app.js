4887c5f98be1f7388e10a72ecafa8bc3db90f5fc
agusdigiam <agustindigiambatista@gmail.com>
2023-01-18 10:28:20 -0300

    first dom

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const main_card = document.querySelector(".main_card");
+const parrafo = document.querySelectorAll(".parrafo");
+const id = document.querySelector("#id");
+const input = document.querySelector("input");
+
+console.log(input.value);
+
+console.log({
+    main_card,
+    parrafo,
+    id,
+    input,
+});
